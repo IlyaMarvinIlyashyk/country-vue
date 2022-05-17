5f6f09b4f573febeb27359e43ca7313d74e534b6
Muhammad Ahmed <mahmed.4zad@gmail.com>
2022-05-17 15:27:26 -0400

    Various components added.
    Signed-off-by: Muhammad Ahmed <mahmed.4zad@gmail.com>

---
+++ src/components/CountryCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <img :src="{ image }" alt="" />
+    <h3>{{ country }}</h3>
+    <h4>{{ continents }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: String,
+    country: String,
+    continents: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card {
+  display: grid;
+  grid-template-columns: 1fr 9fr;
+  margin: 50vh 0 0 0;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav>
+    <a href="/" rel="noopener noreferrer">
+      <span>{{ navTitle }}<br>{{ navSubTitle }}</span>
+    </a>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    navTitle: String,
+    navSubTitle: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+nav {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  background: #081101;
+}
+
+a {
+  display: flex;
+  align-self: flex-start;
+  text-decoration: none;
+  text-align: left;
+}
+
+a > span {
+  color: #aceca1;
+  font-size: 1.35rem;
+  font-weight: 600;
+  padding: 0.85rem;
+}
+</style>
